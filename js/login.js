0127dc60f16b3d3296e734fb5fe876934548e58b
wyx <wyx>
2018-06-26 19:54:04 +0800

    smart living2.0

--- js/login.js
+++ js/login.js
@@ -1,11 +1,23 @@
-let passwardNumber=document.querySelectorAll(".passward-number");
-let numberButton=document.querySelectorAll(".number-button");
-let numberButtonAfter=document.querySelectorAll(".passward-number:after");
-let m=0;
-for(let i=0;i<numberButton.length;i++){
-    numberButton[i].onclick=function () {
-        passwardNumber[m].style.color="#8a5733";
-        // numberButtonAfter[m].style.background="#8a5733";
-        m=++m;
+let passwardNumber = document.querySelectorAll(".passward-number");
+let numberButton = document.querySelectorAll(".number-button");
+let m = 0;
+for (let i = 0; i < numberButton.length; i++) {
+    numberButton[i].onclick = function () {
+        if (i === 9) {
+            passwardNumber[m].style.color = "#fff";
+            m--;
+        }
+        else if (i === 11) {
+            for (m >= 0; m--;) {
+                passwardNumber[m].style.color = "#fff";
+            }
+        }
+        if (m < 0) {
+            m = 0;
+        }
+        else {
+            passwardNumber[m].style.color = "#8a5733";
+            m++;
+        }
     }
 }
